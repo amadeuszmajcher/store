278c0755c91a9b19d49a1e4d19262b1c01304ed5
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2024-01-16 22:45:30 +0100

    feat: discount in cart

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 const product1 = { price: 10, title: "JS od podstaw" };
 const product2 = { price: "20", title: "PHP od podstaw" };
-const discount = 10;
+const discount = 20;
 
 // add products to table
 const itemsContainer = document.querySelector("#items");
@@ -19,12 +19,23 @@ function addItem(item) {
 addItem(product1);
 addItem(product2);
 
+// dodaj znizke
+
+if (discount > 0) {
+  document.querySelector("#discount-amount").innerHTML = -discount;
+  document.querySelector("#discount").classList.remove("hidden");
+}
+
 if (isNaN(+product2.price)) {
   console.log("Podano niepoprawny typ danych");
 }
 
 const total = Number(product1.price) + Number(product2.price);
-const totalwithDiscount = total - 10;
+const totalwithDiscount = total - discount;
+
+// cena całkowita
+
+document.querySelector("#total-price").innerHTML = totalwithDiscount;
 
 console.log(
   `
